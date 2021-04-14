6135f005c559d5c0e90d156735660ce48c78ac1b
Kevin-Souza <78280970+Kevin-Souza@users.noreply.github.com>
2021-04-13 22:35:06 -0300

    add front

--- IMC/IMC/Default.aspx
+++ IMC/IMC/Default.aspx
@@ -17,10 +17,32 @@
                     <h1>Calculadora de IMC</h1>
                 </div>
 
-            <hr />
+                <hr />
 
-            
+                <div class="row">
+                    <div class="col-row-2">
+                        <label>informe sua altura:</label>
+                        <asp:RequiredFieldValidator ID="rfvAltura" ControlToValidate="txtAltura"
+                            ErrorMessage="*" ForeColor="Red" runat="server"></asp:RequiredFieldValidator>
+                        <asp:TextBox ID="txtAltura" runat="server" placeholder="cm" CssClass="form-control"></asp:TextBox>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-row-2" style="padding-top: 10px">
+                        <label>informe seu peso:</label>
+                        <asp:RequiredFieldValidator ID="rfvPeso" ControlToValidate="txtPeso"
+                            ErrorMessage="*" ForeColor="Red" runat="server"></asp:RequiredFieldValidator>
+                        <asp:TextBox ID="txtPeso" runat="server" placeholder="Kg" CssClass="form-control"></asp:TextBox>
+                    </div>
+                </div>
+                <div class="col-row-12" style="padding-top: 20px">
+                    <asp:Button ID="btnCalc" runat="server" Text="Calcular" CssClass="btn-primary" />
+                </div>
+                <div class="col-row-12" style="padding-top: 20px">
+                    <asp:Label ID="lblResu" runat="server" Text=""></asp:Label>
+                </div>
 
+                <hr />
 
             </div>
             <%--container--%>
--- IMC/IMC/Default.aspx.designer.cs
+++ IMC/IMC/Default.aspx.designer.cs
@@ -22,5 +22,59 @@ namespace IMC
         /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Controle rfvAltura.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvAltura;
+
+        /// <summary>
+        /// Controle txtAltura.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtAltura;
+
+        /// <summary>
+        /// Controle rfvPeso.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvPeso;
+
+        /// <summary>
+        /// Controle txtPeso.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPeso;
+
+        /// <summary>
+        /// Controle btnCalc.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnCalc;
+
+        /// <summary>
+        /// Controle lblResu.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblResu;
     }
 }
